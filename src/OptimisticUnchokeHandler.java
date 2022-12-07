049213e5f4dab838bef4d6968fc2bba80e047a69
Dilimulati Diliyaer <diliyaer.d@ufl.edu>
2022-12-07 15:26:53 -0500

    .

--- src/OptimisticUnchokeHandler.java
+++ src/OptimisticUnchokeHandler.java
@@ -1,9 +1,26 @@
 import java.util.HashSet;
+import java.util.Random;
 
-public class OptimisticUnchokeHandler {
+public class OptimisticUnchokeHandler implements Runnable{
     //every x seconds, loop over peerlist and get everyone with weChokedThem true
     //Choose among them who to unchoke
 
-    HashSet<Peer> chokedlist = new HashSet<>(ConfigService.getchoked());
+    public void run() {
+        HashSet<Peer> chokedlist = new HashSet<>(ConfigService.getchoked());
+        int size = chokedlist.size();
+        int item = new Random().nextInt(size);
+        int i = 0;
+        Peer peerChosen;
+        for(Peer obj : chokedlist)
+        {
+            if (i == item)
+                peerChosen = obj;
+            i++;
+        }
+
+        //peerChosen send unchoke
+
+    }
+
 
 }
